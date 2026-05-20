9d603e5071cd86f639e7b04e2edf5b0f37889f98
Acatrinei Robert-Marian <r.acatrinei2005@gmail.com>
2026-05-20 23:59:11 +0300

    changed requirements.txt

--- trainer.py
+++ trainer.py
@@ -4,19 +4,20 @@ from ModelFactory import ModelFactory
 
 MODEL_NAME = "models/test_model_v3.pt"
 
-# factory = ModelFactory(model_size='n')
-# results = factory.fine_tune(
-#     collection_names=[f"day{i:02d}" for i in range(1, 21)]+["unity"],
-#     device="mps",
-#     epochs=10,
-#     batch_size=16,
-#     limit=10000
-# )
-#
-# print("Fine-tuning complete!", flush=True)
-# print("Results:", results)
-#
-# factory.save_model(MODEL_NAME)
+factory = ModelFactory(model_size='n')
+#--index-url https://download.pytorch.org/whl/cu121
+results = factory.fine_tune(
+    collection_names=[f"day{i:02d}" for i in range(1, 21)]+["unity"],
+    device=0,
+    epochs=10,
+    batch_size=16,
+    limit=10000
+)
+
+print("Fine-tuning complete!", flush=True)
+print("Results:", results)
+
+factory.save_model(MODEL_NAME)
 model = YOLO("models/test_model_v3.pt")
 model.to('cpu')
 model.export(format="coreml")
